df7e42239b025cb38cd23641361d2e95245a1ca3
Kickstartlab <info@kickstartlabs.io>
2023-02-15 16:31:52 +0500

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -162,7 +162,7 @@ export default function Home() {
 
                     <div data-aos="flip-left" data-aos-duration="3000" data-aos-delay="50" data-aos-once="false" className="bg-white-100 lg:w-52 border-4 border-seagreen-100 rounded-xl p-5 flex flex-col group hover:bg-blue-50 h-48">
                         <h4 className="text-xl text-blue-100 font-bold text-left group-hover:text-white-100">
-                            5% SELL TAX
+                            Total Supply: 1,000,000,000
                         </h4>
 
                         <a href={require("../assets/whitepaper.pdf")} download="Whitepaper">
@@ -181,6 +181,10 @@ export default function Home() {
                             5% SELL TAX
                         </h4>
 
+                        <p className='text-gray-100 font-work opacity-40 group-hover:text-white-100 text-sm pt-3'>
+                            Total Supply: 1,000,000,000
+                        </p>
+
                         <a href='https://app.uniswap.org/' className='flex gap-2 items-center text-blue-50 font-work pt-5 group-hover:text-white-100'>
                             Buy Now
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
@@ -193,6 +197,10 @@ export default function Home() {
                         <h4 className="text-xl text-blue-100 font-bold text-left group-hover:text-white-100">
                             5% SELL TAX
                         </h4>
+
+                        <p className='text-gray-100 font-work opacity-40 group-hover:text-white-100 text-sm pt-3'>
+                            Total Supply: 1,000,000,000
+                        </p>
 
                         <a href='https://www.dextools.io/app/en/ether/pair-explorer/0xa7214ef7b12eb7445a78fd5e31ed0dd7120f4dce' className='flex gap-2 items-center text-blue-50 font-work pt-5 group-hover:text-white-100'>
                             Chart
